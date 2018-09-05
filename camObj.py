36b3f7c8f7c219438f534a725b557123e7ce2d5e
Bhargav Bhalodi <bhalodib@gmail.com>
2018-09-05 13:46:15 -0400

    Initial commits

---
+++ camObj.py
@@ -0,0 +1,28 @@
+# ----------------------------------------------------------------------------
+# camInfo.py
+# Author: Bhargav Bhalodi
+# Description: Camera Object class to retrieve video information from a camera
+# device.
+# ----------------------------------------------------------------------------
+
+# Camera Object class
+# Creates camera objects for retrieving video information/data from a camera
+# device.
+
+
+class CamObj:
+
+    def __init__(self):
+        # variable 1 - holder for camera object
+        # variable 2 - holder for video data
+        pass
+
+    def set_cam(self):
+        # populates the camera object variable with a camera device
+        # using OpenCV
+        pass
+
+    def get_video_data(self):
+        # populates the video data variable with real video data from the
+        # camera object
+        pass
