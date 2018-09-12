dfff4c065f234753a8adc386724a656b03581741
Bhargav Bhalodi <bhalodib@gmail.com>
2018-09-12 12:50:20 -0400

    Exploring video input and output

--- camObj.py
+++ camObj.py
@@ -8,7 +8,8 @@
 # Camera Object class
 # Creates camera objects for retrieving video information/data from a camera
 # device.
-
+import numpy as np
+import cv2
 
 class CamObj:
 
@@ -17,12 +18,35 @@ class CamObj:
         # variable 2 - holder for video data
         pass
 
-    def set_cam(self):
+    def set_cam(self, newCam):
         # populates the camera object variable with a camera device
         # using OpenCV
         pass
 
-    def get_video_data(self):
-        # populates the video data variable with real video data from the
-        # camera object
-        pass
+    def initialize_video_data(self, camIndex=0, show=False, needRet=False,
+                              filename=''):
+        # initializes video capture from camera at index = camIndex.
+        # displays the capture if show is set to True.
+        # press 'q' to quit the display.
+        # if video data needs to be returned, set needRet to be True and
+        # filename should be provided in the form of fname.avi
+        beginCap = cv2.VideoCapture(camIndex)
+        if needRet:
+            vidWriter = cv2.VideoWriter_fourcc(*'DIVX')
+            outputDestination = cv2.VideoWriter(filename, vidWriter,
+                                                40, (500, 500))
+        while True:
+            check, frames = beginCap.read()
+            if show:
+                cv2.imshow('frame', frames)
+            if needRet:
+                outputDestination.write(frames)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        beginCap.release()
+        cv2.destroyAllWindows()
+
+
+myCam = CamObj()
+myCam.initialize_video_data(0, show=True, needRet=True, filename='trial.avi')
