40584eeaa9b2e5dbdeeea37e97d670062c851f31
Bhargav Bhalodi <bhalodib@gmail.com>
2018-10-31 13:32:32 -0400

    Added initial code to tfModel.py.

--- tfModel.py
+++ tfModel.py
@@ -1,2 +1,92 @@
 # Bhargav Bhalodi
 # Tensorflow model for object detection
+
+# Help taken from the tutorial
+# information given at
+# https://www.tensorflow.org/tutorials/keras/basic_classification
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+cifar10_dataset = keras.datasets.fashion_mnist
+
+(img_train, lab_train), (img_test, lab_test) = cifar10_dataset.load_data()
+
+img_train = img_train/255.0
+img_test = img_test/255.0
+
+plt.figure(figsize=(10,10))
+
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(img_train[i], cmap=plt.cm.binary)
+    plt.xlabel(lab_train[i])
+
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(img_train, lab_train, epochs=5)
+
+test_loss, test_acc = model.evaluate(img_test, lab_test)
+
+print('Test accuracy:', test_acc)
+
+predictions = model.predict(img_test)
+
+
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[i], true_label[i], \
+                                         img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(predicted_label,
+                                         100 * np.max(predictions_array),
+                                         true_label),
+               color=color)
+
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions, lab_test, img_test)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions, lab_test)
